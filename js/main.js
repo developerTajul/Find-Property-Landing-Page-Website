21cb468b6a56b04bcb17acdc425e348c6b034dac
Sakil Anwar <sakilanwar60@gmail.com>
2022-10-05 09:27:00 +0600

    header completed

--- js/main.js
+++ js/main.js
@@ -235,3 +235,21 @@
 	});
 
 }(jQuery));
+
+
+// header area
+
+
+const openMenuBtn = document.querySelector('.header-area .open-menu-btn button');
+const mobileMenu = document.querySelector('.header-area .mobile-menu-area');
+const closeBtn = document.querySelector('.header-area .close-btn button');
+const body = document.querySelector('body');
+
+openMenuBtn.addEventListener('click', function () {
+    mobileMenu.classList.add('toggle-mobile-menu');
+    body.classList.add('bg-gray');
+})
+closeBtn.addEventListener('click', function () {
+    mobileMenu.classList.remove('toggle-mobile-menu');
+    body.classList.remove('bg-gray');
+})
