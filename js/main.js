b3635610c4c16ddf4cdc32ebcedcf20c67fb721f
Sabbir Ahmmed <ahmmedsabbirbd@gmail.com>
2022-10-05 12:24:40 +0600

    header banner complete

--- js/main.js
+++ js/main.js
@@ -2,235 +2,149 @@
 
 	jQuery(document).ready(function(){
 
+        /*** Sticky header */
+		$(window).scroll(function(){
+			if($("body").scrollTop() > 0 || $("html").scrollTop() > 0) {
+				$(".header").addClass("stop");
+			} else {
+				$(".header").removeClass("stop");
+			}
+		});
 
-        // meanmenu
-        jQuery('#mobile-menu').meanmenu({
-            meanMenuContainer: '.mobile-menu',
-            meanScreenWidth: "991"
-        });
+		/*** Sticky header */
+		const body = document.body;
+		const scrollUp = "scroll-up";
+		const scrollDown = "scroll-down";
+		let lastScroll = 100;
 
+		window.addEventListener("scroll", () => {
+			const currentScroll = window.pageYOffset;
+			if (currentScroll <= 0) 
+			{
+				body.classList.remove(scrollUp);
+				return;
+			}
 
-    	/** main-slider **/	
-    	$('.main-slider-active').slick({
-            infinite: true,
-            dots: false,
-            arrows: false,
-            slidesToShow: 1,
-            autoplay: true,
-            autoplaySpeed: 10000,
-            slidesToScroll: 1,
-            nextArrow: '<i class="fa fa-angle-left" aria-hidden="true"></i>',
-            prevArrow: '<i class="fa fa-angle-right" aria-hidden="true"></i>',
-    	});
+			if (currentScroll > lastScroll && !body.classList.contains(scrollDown)) 
+			{
+				// down
+				body.classList.remove(scrollUp);
+				body.classList.add(scrollDown);
+			} 
+			else if ( currentScroll < lastScroll && body.classList.contains(scrollDown) ) 
+			{
+				// up
+				body.classList.remove(scrollDown);
+				body.classList.add(scrollUp);
+			}
 
-        /** gallery-slider **/ 
-        $('.gallery-slider-active').slick({
-            infinite: true,
-            dots: false,
-            arrows: false,
-            slidesToShow: 1,
-            autoplay: true,
-            autoplaySpeed: 10000,
-            slidesToScroll: 1
-        });
+			lastScroll = currentScroll;
+		});
 
-        // portfolio active
-        $('.portfolio-area').imagesLoaded( function() {
-            var grid = $('.grid').isotope({
-                itemSelector: '.grid-item',
-                percentPosition: true,
-                masonry: {
-                    // use outer width of grid-sizer for columnWidth
-                    columnWidth: '.grid-item'
-                }
-            })
+		/*** Scroll Nav */
+		var link = $('.main-menu ul li a');
+ 
+		link.on('click', function(e) {
+			var target = $($(this).attr('href'));
+			$('html, body').animate({
+			scrollTop: target.offset().top - 76
+			}, 600); 
+			$(this).parent().addClass('active');
+			e.preventDefault();
+		});
+ 
+		$(window).on('scroll', function(){
+			scrNav();
+		});
 
-            $('.portfolio-menu').on('click', 'button', function () {
-                var filterValue = $(this).attr('data-filter');
-                grid.isotope({ filter: filterValue });
-            });
+		function scrNav() {
+			var sTop = $(window).scrollTop();
+			$('section').each(function() {
+				var id = $(this).attr('id'),
+					offset = $(this).offset().top-1,
+					height = $(this).height();
+				if(sTop >= offset && sTop < offset + height) {
+					link.parent().removeClass('active');
+					$('.main-menu').find('[href="#' + id + '"]').parent().addClass('active');
+				}
+			});
+		}
+		scrNav();
 
-            //for portfolio menu active class
-            $('.portfolio-menu button').on('click', function (event) {
-                $(this).siblings('.active').removeClass('active');
-                $(this).addClass('active');
-                event.preventDefault();
-            });
+		/*** mobile menu  */
+		$(".hamburger-menu").on("click", function () {
+			$(".side-info").toggleClass("info-open");
+			$(".offcanvas-overlay").toggleClass("overlay-open");
+		});
 
-        });
+		$(window).scroll(function(){
+			if($("body").scrollTop() > 0 || $("html").scrollTop() > 0) {
+				$(".side-info").removeClass("info-open");
+				$(".offcanvas-overlay").removeClass("overlay-open");
+			}
+		});
 
-        // magnificPopup
-        $('.popup-img').magnificPopup({
-            type: 'image',
-            gallery: {
-                enabled: true
-            }
-        });
+        /*** AOS */
+		AOS.init({
+			once: true,
+			offset: 0,
+			duration: 900,
+		});
 
-        // blog - active
-        $('.active-latest-blog').slick({
-            dots: true,
-            arrows: false,
-            infinite: true,
+		/*** enable lightbox */
+		$('.popup-video').magnificPopup({
+			type: 'iframe',
+			preloader: false,
+			fixedBgPos: true,
+			removalDelay: 500,
+			fixedContentPos: true,
+			callbacks: {
+				beforeOpen: function() { 
+					this.st.iframe.markup = this.st.iframe.markup.replace('mfp-iframe-scaler', 'mfp-iframe-scaler mfp-with-anim');
+					this.st.mainClass = this.st.el.attr('data-effect');
+				}
+			},
+			closeMarkup: '<button title="Close (Esc)" type="button" class="mfp-close"></button>',
+		});
+    	
+        // testimonial-active 
+        var testimonials__slider;
+        testimonials__slider = $('.testimonial-area__slider');
+
+        testimonials__slider.slick({
+            dots: true, 
             speed: 300,
+            arrows: true,
+            autoplay: true,
             slidesToShow: 3,
-            slidesToScroll: 3,
-            responsive: [
-                {
-                    breakpoint: 1024,
-                    settings: {
-                        slidesToShow: 2,
-                        slidesToScroll: 1,
-                        infinite: true,
-                        dots: true
-                    }
-                },
-                {
-                    breakpoint: 991,
-                    settings: {
-                        slidesToShow: 2,
-                        slidesToScroll: 1
-                    }
-                },
-                {
-                    breakpoint: 767,
-                    settings: {
-                        slidesToShow: 1,
-                        slidesToScroll: 1
-                    }
-                },
-                {
-                    breakpoint: 480,
-                    settings: {
-                        slidesToShow: 1,
-                        slidesToScroll: 1
-                    }
-                }
-            ]
-        });
-
-        // testimonial-active
-        $('.testimonial-active').slick({
-            dots: true,
-            arrows: false,
             infinite: true,
-            speed: 300,
-            slidesToShow: 2,
             slidesToScroll: 1,
+            autoplaySpeed: 500,
+            dotsClass: "slick-dots list-inline",
+            nextArrow: '<button class="slide-arrow prev-arrow fa-solid fa-arrow-right-long"></button>',
+            prevArrow: '<button class="slide-arrow next-arrow fa-solid fa-arrow-left-long"></button>',
+            appendDots: $(".slider-controls"),
             responsive: [
-                {
-                    breakpoint: 1024,
-                    settings: {
-                        slidesToShow: 2,
-                        slidesToScroll: 1,
-                        infinite: true,
-                        dots: true
-                    }
-                },
-                {
-                    breakpoint: 991,
-                    settings: {
-                        slidesToShow: 2,
-                        slidesToScroll: 1
-                    }
-                },
-                {
-                    breakpoint: 600,
-                    settings: {
-                        slidesToShow: 1,
-                        slidesToScroll: 1
-                    }
-                },
-                {
-                    breakpoint: 480,
-                    settings: {
-                        slidesToShow: 1,
-                        slidesToScroll: 1
-                    }
+            {
+                breakpoint: 768,
+                settings: {
+                 slidesToShow: 2
                 }
-            ]
-        });
-
-        // testimonial-active
-        $('.testimonial-active-3').slick({
-            dots: true,
-            arrows: false,
-            infinite: true,
-            speed: 300,
-            slidesToShow: 1,
-            slidesToScroll: 1,
-        });
-
-
-        // brand-active
-        $('.brand-active').slick({
-            dots: false,
-            arrows: false,
-            infinite: true,
-            speed: 300,
-            slidesToShow: 5,
-            slidesToScroll: 1,
-            responsive: [
-                {
-                    breakpoint: 1024,
-                    settings: {
-                        slidesToShow: 5,
-                        slidesToScroll: 1,
-                        infinite: true,
-                        dots: false
-                    }
-                },
-                {
-                    breakpoint: 991,
-                    settings: {
-                        slidesToShow: 3,
-                        slidesToScroll: 1
-                    }
-                },
-                {
-                    breakpoint: 480,
-                    settings: {
-                        slidesToShow: 1,
-                        slidesToScroll: 1
-                    }
+            },
+            {
+                breakpoint: 530,
+                settings: {
+                    slidesToShow: 1
                 }
+            }
             ]
-        });
+        }); 
 
         /** counter **/
         $('.counter').counterUp({
             delay: 10,
             time: 1000
         });
-
-        // testimonial having two parts / sections
-        $('.testimonia-item-active').slick({
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            arrows: false,
-            fade: true,
-            asNavFor: '.testimonial-nav'
-        });
-        $('.testimonial-nav').slick({
-            slidesToShow: 3,
-            slidesToScroll: 1,
-            asNavFor: '.testimonia-item-active',
-            dots: false,
-            prevArrow:'<button type="button" class="prev"><i class="fas fa-chevron-left"></i></button>',
-            nextArrow:'<button type="button" class="next"><i class="fas fa-chevron-right"></i></button>',
-            centerMode: true,
-            focusOnSelect: true,
-            centerPadding:0,
-            responsive: [
-                {
-                    breakpoint: 480,
-                    settings: {
-                        arrows: false,
-                    }
-                }
-            ]
-        });
-		
 
 	});
 
